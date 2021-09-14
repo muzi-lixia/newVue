ec0d79e414be2842fcd7a0e280e0a763ca16ea36
muzi <muzi_lixia@163.com>
2021-09-14 09:39:59 +0800

    second commit

--- src/Vue.js
+++ src/Vue.js
@@ -1,3 +1,14 @@
+/*
+* vue响应式原理四个步骤：
+*   1、实现一个监听器Observe，对数据对象进行遍历，包括子属性对象的属性，
+*     利用Object.defineProperty()对对象都加上getter和setter，如此，给对象赋值，就能触发getter，监听到数据的变化
+*   2、实现一个解析器Compile，解析vue模板指令，将模板中的变量都替换成数据，然后初始化渲染页面视图，
+*     并将每个指令对应的节点绑定更新函数，添加监听数据的订阅者，一旦数据变动，收到通知，通知更新函数进行数据更新
+*   3、实现一个订阅者Watcher，Watcher是Observe和Compile之间的通信桥梁，主要的任务是订阅Observe中属性变化的消息
+*     当收到属性值变化的消息，触发解析器Compile中对应的更新函数
+*   4、实现一个订阅器Dep，订阅器采用发布-订阅模式，用来收集Watcher，对监听器Observe和解析器Compile实现统一管理
+* */
+
 import Compile from "./Compile";
 import observe from "./observe";
 import Watcher from "./Watcher";
@@ -26,8 +37,8 @@ export default class Vue {
                 get() {
                     return self._data[key];
                 },
-                set(newVlaue) {
-                    self._data[key] = newVlaue;
+                set(newValue) {
+                    self._data[key] = newValue;
                 }
             })
         })
